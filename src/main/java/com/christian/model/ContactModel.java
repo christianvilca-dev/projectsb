966ab4c465657c12363e0d08b39840298f565b05
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-20 18:01:26 -0500

    Controller y Vista - Añadir contacto

--- src/main/java/com/christian/model/ContactModel.java
+++ src/main/java/com/christian/model/ContactModel.java
@@ -59,4 +59,10 @@ public class ContactModel {
 	public void setCity(String city) {
 		this.city = city;
 	}
+
+	@Override
+	public String toString() {
+		return "ContactModel [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", telephone="
+				+ telephone + ", city=" + city + "]";
+	}
 }
