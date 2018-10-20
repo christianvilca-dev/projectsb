16b264dd54c48e14e7d7df56a406b52b30a5200b
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-19 23:52:38 -0500

    Logs

--- src/main/java/com/christian/controller/LoginController.java
+++ src/main/java/com/christian/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.christian.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +13,12 @@ import com.christian.model.UserCredential;
 
 @Controller
 public class LoginController {
+	
+	private static final Log LOG = LogFactory.getLog(LoginController.class);
 
 	@GetMapping("/")
 	public String redirecToLogin() {
+		LOG.info("METHOD: redirecToLogin()");
 		return "redirect:/login";
 	}
 	
@@ -21,19 +26,22 @@ public class LoginController {
 	public String showLoginForm(Model model,
 			@RequestParam(name="error", required=false) String error,
 			@RequestParam(name="logout", required=false) String logout) {
+		LOG.info("METHOD: showLoginForm() -- Param: erro:" +  error + ", logout:"+ logout);
 		model.addAttribute("error", error);
 		model.addAttribute("logout", logout);
 		model.addAttribute("userCredentials", new UserCredential());
+		LOG.info("Returning to login view:");
 		return "login";
 	}
 	
 	@PostMapping("/logincheck")
 	public String loginCheck(@ModelAttribute(name="userCredential") UserCredential userCredential) {
-		
+		LOG.info("METHOD: showLoginForm() -- Params: erro:" + userCredential.toString());
 		if (userCredential.getUsername().equals("user") && userCredential.getPassword().equals("user")) {
+			LOG.info("Returning to contacts view");
 			return "contacts";
-		} else {
-			return "redirect:/login?error";
-		}
+		} 
+		LOG.info("Redirect to login?error");
+		return "redirect:/login?error";
 	}
 }
