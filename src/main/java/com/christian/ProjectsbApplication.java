ce298f1990b01b00867d443be3c15df96ed3d3e5
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-19 22:21:12 -0500

    Inicio

---
+++ src/main/java/com/christian/ProjectsbApplication.java
@@ -0,0 +1,12 @@
+package com.christian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsbApplication.class, args);
+	}
+}
