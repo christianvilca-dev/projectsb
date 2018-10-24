a9e15bfde9e54adc541ff27381b78b0774c669e7
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-23 23:38:50 -0500

    Añadir JavaDoc mediante JAutoDoc

--- src/main/java/com/christian/converter/ContactConverter.java
+++ src/main/java/com/christian/converter/ContactConverter.java
@@ -5,9 +5,24 @@ import org.springframework.stereotype.Component;
 import com.christian.entity.Contact;
 import com.christian.model.ContactModel;
 
+// TODO: Auto-generated Javadoc
+/**
+ * JAutoDoc plugin de eclipse
+ * Permite crear Java docs
+ * 
+ * En el MarketPlace > jautodoc
+ * 
+ * Clase > jautodoc > Add Javadoc
+ */
 @Component("contactConverter")
 public class ContactConverter {
 
+	/**
+	 * Convert contact model 2 contact.
+	 *
+	 * @param contactModel the contact model
+	 * @return the contact
+	 */
 	public Contact convertContactModel2Contact(ContactModel contactModel) {
 		Contact contact = new Contact();
 		contact.setId(contactModel.getId());
@@ -18,6 +33,12 @@ public class ContactConverter {
 		return contact;
 	}
 
+	/**
+	 * Convert contact contact 2 model.
+	 *
+	 * @param contact the contact
+	 * @return the contact model
+	 */
 	public ContactModel convertContactContact2Model(Contact contact) {
 		ContactModel contactModel = new ContactModel();
 		contactModel.setId(contact.getId());
