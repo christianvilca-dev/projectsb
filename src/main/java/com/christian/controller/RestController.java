5dff5f808365d7caea4f066f04e797a82c934fcf
Christian Vilca <christian.vilcaq@gmail.com>
2018-10-23 23:59:29 -0500

    Integracion de Spring Rest 2

--- src/main/java/com/christian/controller/RestController.java
+++ src/main/java/com/christian/controller/RestController.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.christian.model.ContactModel;
+
 /**
  * The Class RestController.
  * Se utiliza en el front-end con una tecnologia que ya no es montar las plantillas en la parte del servidor
@@ -22,7 +24,9 @@ public class RestController {
 	 */
 	// Devuelve un json
 	@GetMapping("/checkrest")
-	public ResponseEntity<String> checkRest() {
-		return new ResponseEntity<String>("OK", HttpStatus.OK);
+	public ResponseEntity<ContactModel> checkRest() {
+		ContactModel cm = new ContactModel(2, "usuario2", "usuario2", "4564654", "Arequipa");
+		return new ResponseEntity<ContactModel>(cm, HttpStatus.OK);
+		// {"id":2,"firstname":"usuario2","lastname":"usuario2","telephone":"4564654","city":"Arequipa"}
 	}
 }
